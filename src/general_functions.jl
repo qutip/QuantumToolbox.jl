2fb59a7afa9d7532664a0ed844062d3964f268d3
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2024-03-15 18:17:11 +0800

    add `partial_transpose` for dense matrices

--- src/general_functions.jl
+++ src/general_functions.jl
@@ -227,6 +227,36 @@ function entropy_vn(ρ::QuantumObject{<:AbstractArray{T},OperatorQuantumObject};
     return -real(sum(nzvals .* logvals))
 end
 
+@doc raw"""
+    partial_transpose(ρ::QuantumObject, mask::Vector{Bool})
+
+Return the partial transpose of a density matrix ``\rho``, where `mask` is an array/vector with length that equals the length of `ρ.dims`. The elements in `mask` are boolean (`true` or `false`) which indicates whether or not the corresponding subsystem should be transposed.
+"""
+function partial_transpose(ρ::QuantumObject{::T, OperatorQuantumObject}, mask::Vector{Bool}) where T
+    if length(mask) != length(ρ.dims) 
+        error("The length of \`mask\` should be equal to the length of \`ρ.dims\`.")
+    end
+    return _partial_transpose(ρ, [1 + Int(i) for i in mask])
+end
+function _partial_transpose(ρ::QuantumObject{<:AbstractMatrix, OperatorQuantumObject}, mask::Vector{Int})
+    # mask has elements with values equal to 1 or 2
+    # 1 - the subsystem don't need to be transposed
+    # 2 - the subsystem need be transposed
+    nsys = length(mask)
+    pt_dims = reshape(Vector(1:(2 * nsys)), (nsys, 2))
+    pt_idx  = [
+        [pt_dims[n,     mask[n]] for n in 1:nsys];
+        [pt_dims[n, 3 - mask[n]] for n in 1:nsys]
+    ]
+    return reshape(
+        permutedims(
+            reshape(ρ.data, (ρ.dims..., ρ.dims...)), 
+            pt_idx
+        ), 
+        size(ρ)
+    )
+end
+
 """
     entanglement(QO::QuantumObject, sel::Vector)
 
