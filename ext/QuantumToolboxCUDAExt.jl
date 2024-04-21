94dd75eaae7f75cdc2ba9fdbaada205d97d48aff
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2024-04-21 11:19:01 +0800

    update `CUDA` extension

--- ext/QuantumToolboxCUDAExt.jl
+++ ext/QuantumToolboxCUDAExt.jl
@@ -1,34 +1,19 @@
 module QuantumToolboxCUDAExt
 
 using QuantumToolbox
-import Sys
 import CUDA: cu, CuArray
 import CUDA.CUSPARSE: CuSparseVector, CuSparseMatrixCSC, CuSparseMatrixCSR
 import SparseArrays: SparseVector, SparseMatrixCSC
 
-cu(A::QuantumObject; word_size::Int=Sys.WORD_SIZE) = ((word_size == 64) || (word_size == 32)) ? cu(A, Val(word_size)) : throw(DomainError(word_size, "The word size should be 32 or 64."))
+CuArray{T}(A::QuantumObject{Tq}) where {T,Tq<:Union{Vector,Matrix}} = QuantumObject(CuArray{T}(A.data), A.type, A.dims)
+CuSparseVector{T}(A::QuantumObject{<:SparseVector}) where T = QuantumObject(CuSparseVector{T}(A.data), A.type, A.dims)
+CuSparseMatrixCSC{T}(A::QuantumObject{<:SparseMatrixCSC}) where T = QuantumObject(CuSparseMatrixCSC{T}(A.data), A.type, A.dims)
+CuSparseMatrixCSR{T}(A::QuantumObject{<:SparseMatrixCSC}) where T = QuantumObject(CuSparseMatrixCSR{T}(A.data), A.type, A.dims)
 
-cu(A::QuantumObject{T}, word_size::TW) where 
-{T<:Union{Vector,Matrix},TW<:Union{Val{32},Val{64}}} = QuantumObject(CuArray{_change_eltype(eltype(A), word_size)}(A.data), A.type, A.dims)
-
-function cu(A::QuantumObject{<:SparseVector}, word_size::TW) where TW<:Union{Val{32},Val{64}}
-    A_cpu  = A.data
-    Ti     = _change_eltype(Int, word_size)
-    Tv     = _change_eltype(eltype(A_cpu), word_size)
-    rowval = CuArray{Ti}(A_cpu.nzind)
-    nzval  = CuArray{Tv}(A_cpu.nzval)
-    return QuantumObject(CuSparseVector{Tv, Ti}(rowval, nzval, A_cpu.n), A.type, A.dims)
-end
-
-function cu(A::QuantumObject{<:SparseMatrixCSC}, word_size::TW) where TW<:Union{Val{32},Val{64}}
-    A_cpu  = A.data
-    Ti     = _change_eltype(Int, word_size)
-    Tv     = _change_eltype(eltype(A_cpu), word_size)
-    colptr = CuArray{Ti}(A_cpu.colptr)
-    rowval = CuArray{Ti}(A_cpu.rowval)
-    nzval  = CuArray{Tv}(A_cpu.nzval)
-    return QuantumObject(CuSparseMatrixCSC{Tv, Ti}(colptr, rowval, nzval, size(A_cpu)), A.type, A.dims)
-end
+cu(A::QuantumObject; word_size::Int=32) = ((word_size == 64) || (word_size == 32)) ? cu(A, Val(word_size)) : throw(DomainError(word_size, "The word size should be 32 or 64."))
+cu(A::QuantumObject{T}, word_size::TW) where {T<:Union{Vector,Matrix},TW<:Union{Val{32},Val{64}}} = CuArray{_change_eltype(eltype(A), word_size)}(A)
+cu(A::QuantumObject{<:SparseVector}, word_size::TW) where TW<:Union{Val{32},Val{64}} = CuSparseVector{_change_eltype(eltype(A), word_size)}(A)
+cu(A::QuantumObject{<:SparseMatrixCSC}, word_size::TW) where TW<:Union{Val{32},Val{64}} = CuSparseMatrixCSC{_change_eltype(eltype(A), word_size)}(A)
 
 _change_eltype(::Type{T}, ::Val{64}) where {T<:Int} = Int64
 _change_eltype(::Type{T}, ::Val{32}) where {T<:Int} = Int32
