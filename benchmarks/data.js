c58ed70b1be6114a42a990ed33af254f799d4562
github-action-benchmark <github@users.noreply.github.com>
2024-05-05 10:02:40 +0000

    add Benchmark results (julia) benchmark result for c79aae033cf1d92e3ded58411b3ceb740118a10c

---
+++ benchmarks/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1714903360870,
+  "repoUrl": "https://github.com/albertomercurio/QuantumToolbox.jl",
+  "entries": {
+    "Benchmark results": [
+      {
+        "commit": {
+          "author": {
+            "email": "61953577+albertomercurio@users.noreply.github.com",
+            "name": "Alberto Mercurio",
+            "username": "albertomercurio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c79aae033cf1d92e3ded58411b3ceb740118a10c",
+          "message": "Merge pull request #96 from albertomercurio/dev/patch-3\n\nAdd benchmark tracking",
+          "timestamp": "2024-05-05T11:56:44+02:00",
+          "tree_id": "3f7caf2742be57bc22b286a04ae0d2138fbe04a3",
+          "url": "https://github.com/albertomercurio/QuantumToolbox.jl/commit/c79aae033cf1d92e3ded58411b3ceb740118a10c"
+        },
+        "date": 1714903359775,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "steadystate/driven-dissipative harmonic oscillator",
+            "value": 7402800.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=8686048\nallocs=250\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
