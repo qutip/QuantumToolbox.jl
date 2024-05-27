70c30bbc88748df46d2a1066433669586ce23bed
Alberto Mercurio <alberto.mercurio96@gmail.com>
2024-05-27 13:44:28 +0200

    Minor changes

--- docs/make.jl
+++ docs/make.jl
@@ -14,8 +14,8 @@ const PAGES = [
             "users_guide/QuantumObject/QuantumObject.md",
             "users_guide/QuantumObject/QuantumObject_functions.md",
         ],
-        "Manipulating States and Operators" => "users_guide/states_and_operators/states_and_operators.md",
-        "Tensor Products and Partial Traces" => "users_guide/tensor_product/tensor.md",
+        "Manipulating States and Operators" => "users_guide/states_and_operators.md",
+        "Tensor Products and Partial Traces" => "users_guide/tensor.md",
         "Time Evolution and Dynamics" => [
             "Introduction" => "users_guide/time_evolution/intro.md"
         ],
