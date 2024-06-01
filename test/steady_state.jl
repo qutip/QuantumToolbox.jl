aaeea74f9e019db5372f86c9b3d5bd6381c8231d
samu-sys <samunapoli67@gmail.com>
2024-06-01 18:00:53 +0200

    Added steadystate_floquet function

--- test/steady_state.jl
+++ test/steady_state.jl
@@ -31,9 +31,14 @@
     c_ops = [sqrt(0.1) * a]
     e_ops = [a_d * a]
     psi0 = fock(N, 3)
-    t_l = LinRange(0, 200, 1000)
+    t_l = LinRange(0, 100 * 2π, 1000)
     H_t_f = TimeDependentOperatorSum([(t, p) -> sin(t)], [liouvillian(H_t)])
     sol_me = mesolve(H, psi0, t_l, c_ops, e_ops = e_ops, H_t = H_t_f, alg = Vern7(), progress_bar = false)
-    ρ_ss = steadystate_floquet(H, c_ops, -1im * 0.5 * H_t, 1im * 0.5 * H_t, 1)
-    @test abs(sum(sol_me.expect[1, end-100:end]) / 101 - expect(e_ops[1], ρ_ss)) < 1e-2
+    ρ_ss1 = steadystate_floquet(H, -1im * 0.5 * H_t, 1im * 0.5 * H_t, 1, c_ops, solver=SSFloquetLinearSystem())[1]
+    ρ_ss2 = steadystate_floquet(H, -1im * 0.5 * H_t, 1im * 0.5 * H_t, 1, c_ops, solver=SSFloquetEffectiveLiouvillian())
+
+    @test tracedist(sol_me.states[end], ρ_ss1) < 1e-4
+    @test tracedist(sol_me.states[end], ρ_ss2) < 1e-4
+
+    # @test abs(sum(sol_me.expect[1, end-100:end]) / 101 - expect(e_ops[1], ρ_ss)) < 1e-2
 end
