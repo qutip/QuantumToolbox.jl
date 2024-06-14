3ad343f0e52828dec9721458364f0430163089e4
Alberto Mercurio <alberto.mercurio96@gmail.com>
2024-06-14 15:21:46 +0200

    Uncomment runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,21 +8,21 @@ const testdir = dirname(@__FILE__)
 
 # Put core tests in alphabetical order
 core_tests = [
-    # "correlations_and_spectrum.jl",
-    # "dynamical_fock_dimension_mesolve.jl",
-    # "dynamical-shifted-fock.jl",
-    # "eigenvalues_and_operators.jl",
-    # "entanglement.jl",
-    # "generalized_master_equation.jl",
-    # "low_rank_dynamics.jl",
-    # "negativity_and_partial_transpose.jl",
-    # "permutation.jl",
+    "correlations_and_spectrum.jl",
+    "dynamical_fock_dimension_mesolve.jl",
+    "dynamical-shifted-fock.jl",
+    "eigenvalues_and_operators.jl",
+    "entanglement.jl",
+    "generalized_master_equation.jl",
+    "low_rank_dynamics.jl",
+    "negativity_and_partial_transpose.jl",
+    "permutation.jl",
     "progress_bar.jl",
-    # "quantum_objects.jl",
-    # "states_and_operators.jl",
-    # "steady_state.jl",
-    # "time_evolution_and_partial_trace.jl",
-    # "wigner.jl",
+    "quantum_objects.jl",
+    "states_and_operators.jl",
+    "steady_state.jl",
+    "time_evolution_and_partial_trace.jl",
+    "wigner.jl",
 ]
 
 if ((GROUP == "All") || (GROUP == "Code-Quality")) && (VERSION >= v"1.9")
