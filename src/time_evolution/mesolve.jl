8763a207a16bf42171885a2b12d354fb2e2bcfd4
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2024-06-16 00:36:53 +0800

    format files

--- src/time_evolution/mesolve.jl
+++ src/time_evolution/mesolve.jl
@@ -96,7 +96,8 @@ function mesolveProblem(
 }
     H.dims != ψ0.dims && throw(DimensionMismatch("The two quantum objects are not of the same Hilbert dimension."))
 
-    haskey(kwargs, :save_idxs) && throw(ArgumentError("The keyword argument \"save_idxs\" is not supported in QuantumToolbox."))
+    haskey(kwargs, :save_idxs) &&
+        throw(ArgumentError("The keyword argument \"save_idxs\" is not supported in QuantumToolbox."))
 
     is_time_dependent = !(H_t === nothing)
 
