c22f230e21dd3d479840446e5a78d45f1cc846c7
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2024-07-08 23:52:52 +0800

    optimize the generation of Ginibre ensemble

--- src/utilities.jl
+++ src/utilities.jl
@@ -48,4 +48,4 @@ end
 _get_dense_similar(A::AbstractArray, args...) = similar(A, args...)
 _get_dense_similar(A::AbstractSparseMatrix, args...) = similar(nonzeros(A), args...)
 
-_Ginibre_ensemble(n::Int, rank::Int = n) = (randn(n, rank) + 1.0im * randn(n, rank)) / sqrt(2 * n)
+_Ginibre_ensemble(n::Int, rank::Int = n) = randn(ComplexF64, n, rank) / sqrt(n)
