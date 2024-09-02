791b6a5d43d6cc0a416a413abd05f51e1e32cb2a
Alberto Mercurio <61953577+albertomercurio@users.noreply.github.com>
2024-09-02 15:59:34 +0200

    Improve ProgressBar thread safety (#212)

--- src/progress_bar.jl
+++ src/progress_bar.jl
@@ -1,18 +1,29 @@
 export ProgressBar, next!
 
-mutable struct ProgressBar{CT,T1<:Integer,T2<:Real,T3<:Real}
+struct ProgressBar{CT,T1<:Integer,T2<:Real,T3,T4<:Real,LT}
     counter::CT
     max_counts::T1
     enable::Bool
     bar_width::T1
     start_time::T2
-    previous_time::T2
-    interval::T3
+    previous_time::T3
+    interval::T4
+    lock::LT
 end
 
 function ProgressBar(max_counts::Int; enable::Bool = true, bar_width::Int = 30, interval = 1.0)
     start_time = time()
-    return ProgressBar(Threads.Atomic{Int}(0), max_counts, enable, bar_width, start_time, start_time, interval)
+    lock = ReentrantLock()
+    return ProgressBar(
+        Threads.Atomic{Int}(0),
+        max_counts,
+        enable,
+        bar_width,
+        start_time,
+        Threads.Atomic{Float64}(start_time),
+        interval,
+        lock,
+    )
 end
 
 function next!(p::ProgressBar, io::IO = stdout)
@@ -26,19 +37,21 @@ function next!(p::ProgressBar, io::IO = stdout)
     max_counts = p.max_counts
     bar_width = p.bar_width
     start_time = p.start_time
-    previous_time = p.previous_time
+    previous_time = p.previous_time[]
     interval = p.interval
 
-    ((time() - previous_time) < interval && counter != p.max_counts) && return
+    current_time = time()
 
-    p.previous_time = time()
+    ((current_time - previous_time) < interval && counter != p.max_counts) && return
+
+    Threads.atomic_xchg!(p.previous_time, current_time)
 
     percentage = counter / max_counts
     percentage_100 = lpad(round(100 * percentage, digits = 1), 5, " ")
     progress = floor(Int, bar_width * percentage)
 
     # Calculate the elapsed time in seconds
-    elapsed_time = floor(Int, time() - start_time)
+    elapsed_time = floor(Int, current_time - start_time)
     # Convert the elapsed time into a string in hours, minutes and seconds
     elapsed_time_str = string(
         elapsed_time ÷ 3600,
@@ -57,11 +70,16 @@ function next!(p::ProgressBar, io::IO = stdout)
     # Construct the progress bar string
     bar = "[" * repeat("=", progress) * repeat(" ", bar_width - progress) * "]"
 
-    print(io, "\rProgress: $bar $percentage_100% --- Elapsed Time: $elapsed_time_str (ETA: $eta_str)")
+    lock(p.lock)
+    try
+        print(io, "\rProgress: $bar $percentage_100% --- Elapsed Time: $elapsed_time_str (ETA: $eta_str)")
 
-    counter == p.max_counts && print(io, "\n")
+        counter == p.max_counts && print(io, "\n")
 
-    flush(io)
+        flush(io)
+    finally
+        unlock(p.lock)
+    end
 
     return nothing
 end
