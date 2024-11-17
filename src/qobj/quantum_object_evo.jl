b652fa782cc50f5b4f8d9c6b3b7d33038c63d209
Alberto Mercurio <alberto.mercurio96@gmail.com>
2024-11-17 09:28:04 +0100

    Use Base allequal function

--- src/qobj/quantum_object_evo.jl
+++ src/qobj/quantum_object_evo.jl
@@ -214,8 +214,6 @@ function QuantumObjectEvolution(
     return QuantumObjectEvolution(α * op.data, type, op.dims)
 end
 
-_all_equal(dims) = all(x -> x == first(dims), dims)
-
 #=
     _QobjEvo_generate_data(op_func_list::Tuple, α; f::Function=identity)
 
@@ -271,7 +269,7 @@ Parse the `op_func_list` and generate the data for the `QuantumObjectEvolution` 
     quote
         dims = tuple($(dims_expr...))
 
-        _all_equal(dims) || throw(ArgumentError("The dimensions of the operators must be the same."))
+        allequal(dims) || throw(ArgumentError("The dimensions of the operators must be the same."))
 
         data_expr_const = $qobj_expr_const isa Integer ? $qobj_expr_const : _make_SciMLOperator($qobj_expr_const, α)
 
