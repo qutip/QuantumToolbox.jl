34e6c499e3d70cc0910cf7d5ffb2cafc87ac7076
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2024-12-03 15:47:09 +0900

    fix benchmark

--- benchmarks/correlations_and_spectrum.jl
+++ benchmarks/correlations_and_spectrum.jl
@@ -1,3 +1,9 @@
+function _calculate_fft_spectrum(H, tlist, c_ops, A, B)
+    corr = correlation_2op_1t(H, nothing, tlist, c_ops, A, B; progress_bar = Val(false))
+    ωlist, spec = spectrum_correlation_fft(tlist, corr)
+    return nothing
+end
+
 function benchmark_correlations_and_spectrum!(SUITE)
     N = 15
     ω = 1
@@ -12,12 +18,6 @@ function benchmark_correlations_and_spectrum!(SUITE)
     t_l = range(0, 333 * π, length = 1000)
 
     PI_solver = PseudoInverse()
-
-    function _calculate_fft_spectrum(H, tlist, c_ops, A, B)
-        corr = correlation_2op_1t(H, nothing, tlist, c_ops, A, B; progress_bar = Val(false))
-        ωlist, spec = spectrum_correlation_fft(tlist, corr)
-        return nothing
-    end
 
     SUITE["Correlations and Spectrum"]["FFT Correlation"] =
         @benchmarkable _calculate_fft_spectrum($H, $t_l, $c_ops, $(a'), $a)
