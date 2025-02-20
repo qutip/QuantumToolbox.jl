c30cdfcccf83256dcad1a0ac285fe24e002f4bb6
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2025-02-20 16:06:35 +0900

    add reference for `hilbert_dist`

--- src/metrics.jl
+++ src/metrics.jl
@@ -47,6 +47,9 @@ Calculates the Hilbert-Schmidt distance between two [`QuantumObject`](@ref):
 ``D_{HS}(\hat{\rho}, \hat{\sigma}) = \textrm{Tr}\left[\hat{A}^\dagger \hat{A}\right]``, where ``\hat{A} = \hat{\rho} - \hat{\sigma}``.
 
 Note that `ρ` and `σ` must be either [`Ket`](@ref) or [`Operator`](@ref).
+
+# References
+- [Vedral-Plenio1998](@citet)
 """
 function hilbert_dist(
     ρ::QuantumObject{ObjType1},
