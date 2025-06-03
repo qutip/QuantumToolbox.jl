c12ba94240040a6a91de8a6b26b4c0bef6736b7f
cailxun <cai.l.xun@gmail.com>
2025-06-03 10:02:41 +0800

    formatting

--- src/br.jl
+++ src/br.jl
@@ -25,24 +25,24 @@ The return depends on `fock_basis`.
 - `fock_basis=Val(false)`: return the Bloch-Redfield tensor (in the eigen basis) along with the transformation matrix from eigen to fock basis.
 """
 function bloch_redfield_tensor(
-        H::QuantumObject{Operator},
-        a_ops::Union{AbstractVector, Tuple},
-        c_ops::Union{AbstractVector, Tuple, Nothing}=nothing;
-        sec_cutoff::Real=0.1,
-        fock_basis::Union{Val,Bool}=Val(false)
-    )
+    H::QuantumObject{Operator},
+    a_ops::Union{AbstractVector,Tuple},
+    c_ops::Union{AbstractVector,Tuple,Nothing} = nothing;
+    sec_cutoff::Real = 0.1,
+    fock_basis::Union{Val,Bool} = Val(false),
+)
     rst = eigenstates(H)
     U = QuantumObject(rst.vectors, Operator(), H.dimensions)
     sec_cutoff = float(sec_cutoff)
 
     # in fock basis
     R0 = liouvillian(H, c_ops)
-    
+
     # set fock_basis=Val(false) and change basis together at the end
     R1 = 0
     if !isempty(a_ops)
-        R1 += mapreduce(x -> _brterm(rst, x[1], x[2], sec_cutoff, Val(false)), +, a_ops) 
-        
+        R1 += mapreduce(x -> _brterm(rst, x[1], x[2], sec_cutoff, Val(false)), +, a_ops)
+
         # do (a_op, spectra)
         #     _brterm(rst, a_op, spectra, sec_cutoff, Val(false))
         # end
@@ -83,12 +83,12 @@ The return depends on `fock_basis`.
 - `fock_basis=Val(false)`: return the Bloch-Redfield term (in the eigen basis) along with the transformation matrix from eigen to fock basis.
 """
 function brterm(
-        H::QuantumObject{Operator},
-        a_op::QuantumObject{Operator}, 
-        spectra::Function;
-        sec_cutoff::Real=0.1, 
-        fock_basis::Union{Bool, Val}=Val(false)
-    )
+    H::QuantumObject{Operator},
+    a_op::QuantumObject{Operator},
+    spectra::Function;
+    sec_cutoff::Real = 0.1,
+    fock_basis::Union{Bool,Val} = Val(false),
+)
     rst = eigenstates(H)
     term = _brterm(rst, a_op, spectra, sec_cutoff, makeVal(fock_basis))
     if getVal(fock_basis)
@@ -99,20 +99,19 @@ function brterm(
 end
 
 function _brterm(
-        rst::EigsolveResult,
-        a_op::T,
-        spectra::F,
-        sec_cutoff::Real,
-        fock_basis::Union{Val{true},Val{false}}
-    ) where {T<:QuantumObject{Operator},F<:Function}
-
+    rst::EigsolveResult,
+    a_op::T,
+    spectra::F,
+    sec_cutoff::Real,
+    fock_basis::Union{Val{true},Val{false}},
+) where {T<:QuantumObject{Operator},F<:Function}
     _check_br_spectra(spectra)
 
-    U, N = rst.vectors, prod(rst.dimensions) 
-    
+    U, N = rst.vectors, prod(rst.dimensions)
+
     skew = @. rst.values - rst.values' |> real
     spectrum = spectra.(skew)
-    
+
     A_mat = U' * a_op.data * U
 
     ac_term = (A_mat .* spectrum) * A_mat
@@ -128,16 +127,14 @@ function _brterm(
         vec_skew = vec(skew)
         M_cut = @. abs(vec_skew - vec_skew') < sec_cutoff
     end
-    
-    out = 0.5 * (
-        + _sprepost(A_mat .* trans(spectrum), A_mat)
-        + _sprepost(A_mat, A_mat .* spectrum)
-        - _spost(ac_term, Id) 
-        - _spre(bd_term, Id)
-    )
+
+    out =
+        0.5 * (
+            + _sprepost(A_mat .* trans(spectrum), A_mat) + _sprepost(A_mat, A_mat .* spectrum) - _spost(ac_term, Id) -
+            _spre(bd_term, Id)
+        )
 
     (sec_cutoff != -1) && (out .*= M_cut)
-    
 
     if getVal(fock_basis)
         SU = _sprepost(U, U')
@@ -146,8 +143,6 @@ function _brterm(
         return QuantumObject(out, SuperOperator(), rst.dimensions)
     end
 end
-
-
 
 @doc raw"""
     brmesolve(
@@ -183,25 +178,23 @@ Solves for the dynamics of a system using the Bloch-Redfield master equation, gi
 - `sol::TimeEvolutionSol`: The solution of the time evolution. See also [`TimeEvolutionSol`](@ref)
 """
 function brmesolve(
-        H::QuantumObject{Operator}, 
-        ψ0::QuantumObject,
-        tlist::AbstractVector, 
-        a_ops::Union{Nothing, AbstractVector, Tuple}, 
-        c_ops::Union{Nothing, AbstractVector, Tuple}=nothing;
-        sec_cutoff::Real=0.1,
-        e_ops::Union{Nothing, AbstractVector}=nothing,
-        kwargs...,
-    )
-    
-    R = bloch_redfield_tensor(H, a_ops, c_ops; sec_cutoff=sec_cutoff, fock_basis=Val(true))
+    H::QuantumObject{Operator},
+    ψ0::QuantumObject,
+    tlist::AbstractVector,
+    a_ops::Union{Nothing,AbstractVector,Tuple},
+    c_ops::Union{Nothing,AbstractVector,Tuple} = nothing;
+    sec_cutoff::Real = 0.1,
+    e_ops::Union{Nothing,AbstractVector} = nothing,
+    kwargs...,
+)
+    R = bloch_redfield_tensor(H, a_ops, c_ops; sec_cutoff = sec_cutoff, fock_basis = Val(true))
 
-    return mesolve(R, ψ0, tlist, nothing; e_ops=e_ops, kwargs...)
+    return mesolve(R, ψ0, tlist, nothing; e_ops = e_ops, kwargs...)
 end
 
 function _check_br_spectra(f::Function)
     meths = methods(f, [Real])
-    length(meths.ms) == 0 && throw(
-        ArgumentError("The following function must accept one argument: `$(meths.mt.name)(ω)` with ω<:Real")        
-    )
+    length(meths.ms) == 0 &&
+        throw(ArgumentError("The following function must accept one argument: `$(meths.mt.name)(ω)` with ω<:Real"))
     return nothing
 end
