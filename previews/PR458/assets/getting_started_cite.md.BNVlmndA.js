59373bd0e1fb301a5ee6e1d760dc38edb5350f48
Documenter.jl <documenter@juliadocs.github.io>
2025-09-03 10:39:09 +0000

    delete history

---
+++ previews/PR458/assets/getting_started_cite.md.BNVlmndA.js
@@ -0,0 +1,11 @@
+import{_ as n,c as e,o as s,ai as t}from"./chunks/framework.mnwdkPdu.js";const _=JSON.parse('{"title":"Cite QuantumToolbox.jl","description":"","frontmatter":{},"headers":[],"relativePath":"getting_started/cite.md","filePath":"getting_started/cite.md","lastUpdated":null}'),i={name:"getting_started/cite.md"};function p(o,a,l,r,c,u){return s(),e("div",null,a[0]||(a[0]=[t(`<h1 id="doc:Cite" tabindex="-1">Cite QuantumToolbox.jl <a class="header-anchor" href="#doc:Cite" aria-label="Permalink to &quot;Cite QuantumToolbox.jl {#doc:Cite}&quot;">​</a></h1><p>If you like <code>QuantumToolbox.jl</code>, we would appreciate it if you could cite our arXiv preprint [ <a href="https://doi.org/10.48550/arXiv.2504.21440" target="_blank" rel="noreferrer">arXiv:2504.21440 (2025)</a> ] using the following bibtex entry:</p><div class="language-bib vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bib</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>@article{QuantumToolbox-jl2025,</span></span>
+<span class="line"><span>    title={{QuantumToolbox.jl}: An efficient {Julia} framework for simulating open quantum systems},</span></span>
+<span class="line"><span>    author={Mercurio, Alberto and Huang, Yi-Te and Cai, Li-Xun and Chen, Yueh-Nan and Savona, Vincenzo and Nori, Franco},</span></span>
+<span class="line"><span>    journal={arXiv preprint arXiv:2504.21440},</span></span>
+<span class="line"><span>    year={2025},</span></span>
+<span class="line"><span>    publisher = {arXiv},</span></span>
+<span class="line"><span>    eprint={2504.21440},</span></span>
+<span class="line"><span>    archivePrefix={arXiv},</span></span>
+<span class="line"><span>    primaryClass={quant-ph},</span></span>
+<span class="line"><span>    doi = {10.48550/arXiv.2504.21440}</span></span>
+<span class="line"><span>}</span></span></code></pre></div>`,3)]))}const h=n(i,[["render",p]]);export{_ as __pageData,h as default};
