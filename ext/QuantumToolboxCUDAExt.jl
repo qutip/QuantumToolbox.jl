c1b09d3e070691f8ff13167b45104ddab936c2eb
Alberto Mercurio <alberto.mercurio96@gmail.com>
2026-01-04 12:47:15 +0100

    Align with latest changes in CUDA

--- ext/QuantumToolboxCUDAExt.jl
+++ ext/QuantumToolboxCUDAExt.jl
@@ -4,7 +4,8 @@ using QuantumToolbox
 using QuantumToolbox: makeVal, getVal
 import QuantumToolbox: _sparse_similar, _convert_eltype_wordsize
 import CUDA: cu, CuArray, allowscalar
-import CUDA.CUSPARSE: CuSparseVector, CuSparseMatrixCSC, CuSparseMatrixCSR, AbstractCuSparseArray
+import CUDA.CUSPARSE: CuSparseVector, CuSparseMatrixCSC, CuSparseMatrixCSR
+import GPUArrays: AbstractGPUSparseArray
 import SparseArrays: SparseVector, SparseMatrixCSC, sparse
 import CUDA.Adapt: adapt
 
@@ -97,10 +98,10 @@ function cu(
     return CuSparseMatrixCSC{_convert_eltype_wordsize(eltype(A), word_size)}(A)
 end
 
-QuantumToolbox.to_dense(A::MT) where {MT <: AbstractCuSparseArray} = CuArray(A)
+QuantumToolbox.to_dense(A::MT) where {MT <: AbstractGPUSparseArray} = CuArray(A)
 
 QuantumToolbox.to_dense(::Type{T1}, A::CuArray{T2}) where {T1 <: Number, T2 <: Number} = CuArray{T1}(A)
-QuantumToolbox.to_dense(::Type{T}, A::AbstractCuSparseArray) where {T <: Number} = CuArray{T}(A)
+QuantumToolbox.to_dense(::Type{T}, A::AbstractGPUSparseArray) where {T <: Number} = CuArray{T}(A)
 
 QuantumToolbox._sparse_similar(A::CuSparseMatrixCSC, args...) = sparse(args..., fmt = :csc)
 QuantumToolbox._sparse_similar(A::CuSparseMatrixCSR, args...) = sparse(args..., fmt = :csr)
