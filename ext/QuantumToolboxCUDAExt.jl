aea6ecb183d0dbcbac40bace3e4c09bc3b142584
Alberto Mercurio <alberto.mercurio96@gmail.com>
2026-01-07 23:57:25 +0100

    Remove CuSparseMatrixCOO

--- ext/QuantumToolboxCUDAExt.jl
+++ ext/QuantumToolboxCUDAExt.jl
@@ -4,7 +4,7 @@ using QuantumToolbox
 using QuantumToolbox: makeVal, getVal
 import QuantumToolbox: _sparse_similar, _convert_eltype_wordsize
 import CUDA: cu, CuArray, allowscalar
-import CUDA.CUSPARSE: CuSparseVector, CuSparseMatrixCSC, CuSparseMatrixCSR, CuSparseMatrixCOO
+import CUDA.CUSPARSE: CuSparseVector, CuSparseMatrixCSC, CuSparseMatrixCSR
 import SparseArrays: SparseVector, SparseMatrixCSC, sparse
 import CUDA.Adapt: adapt
 
