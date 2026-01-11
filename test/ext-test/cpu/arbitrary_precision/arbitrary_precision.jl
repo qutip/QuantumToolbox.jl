4d065bef8d53730a0e8ce9462c22438f237d8b16
Alberto Mercurio <alberto.mercurio96@gmail.com>
2026-01-11 02:29:57 +0100

    Fix test

--- test/ext-test/cpu/arbitrary_precision/arbitrary_precision.jl
+++ test/ext-test/cpu/arbitrary_precision/arbitrary_precision.jl
@@ -71,7 +71,7 @@
         end
 
         @test vals_al[1:(end - 1)] ≈ vals_big_al[idxs_al][1:(end - 1)] atol = 1.0e-6
-        @test all(zip(vecs_al, vecs_big_al[idxs_al])) do (v, v_big)
+        @test all(zip(vecs_al[1:(end - 1)], vecs_big_al[idxs_al][1:(end - 1)])) do (v, v_big)
             return isapprox(abs(dot(v.data, v_big.data)), 1; atol = 1.0e-6)
         end
     end
