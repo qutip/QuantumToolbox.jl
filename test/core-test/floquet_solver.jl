da7dba3603bd7b357d9652fa1db0aef8d6ec91e4
bibekbh1 <bibekbhandari018@gmail.com>
2026-01-14 12:43:32 -0800

    added the first test comparing sesolve and floquet

--- test/core-test/floquet_solver.jl
+++ test/core-test/floquet_solver.jl
@@ -11,8 +11,13 @@ using Random
     H1 = a + a_d
     H_tuple = (H0,(H1,coef))
     T = 2 * π
-    tlist = range(0.0, 2T, length=101)
+    tlist = range(0.0, 3T, length=101)
     floquet_basis = FloquetBasis(H_tuple, T, tlist)
-
-    
+    psi0 = rand_ket(N)
+    floquet_psi0 = to_floquet_basis(floquet_basis, psi0)
+    sol = sesolve(H_tuple, psi0, tlist, e_ops = [], saveat = tlist)
+    states = sol.states
+    for (t,state) in zip(tlist,states)
+        from_floquet = from_floquet_basis(floquet_basis, floquet_psi0, t)
+        @test overlap(state, from_floquet) ≈ 1.0 atol=8e-5  
 end
