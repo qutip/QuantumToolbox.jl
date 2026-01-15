b15b513bfd0eb3a07930ca049d1f6bc14895b206
bibekbh1 <bibekbhandari018@gmail.com>
2026-01-15 10:35:19 -0800

    added the name type in functions and another test set

--- test/core-test/floquet_solver.jl
+++ test/core-test/floquet_solver.jl
@@ -2,18 +2,17 @@ using QuantumToolbox
 using Test 
 using Random
 
-function _matrix_element(c_op_fmmesolve, vp,ep)
+function _matrix_element(c_op_fmmesolve::AbstractQuantumObject, vp::AbstractVector,ep::AbstractVector)
     return vp' * c_op_fmmesolve * ep
 end
 
-function _convert_c_ops(c_op_fmmesolve, noise_spectrum, vp, ep)
+function _convert_c_ops(c_op_fmmesolve::AbstractQuantumObject, noise_spectrum::Real, vp::AbstractVector, ep::AbstractVector)
     c_op_mesolve = []
     N = length(vp)
     for i in 1:N
         for j in 1:N
             if i != j
                 #calculate the rate 
-
                 gamma = 2 * np.pi * _matrix_element(c_op_fmmesolve, vp[j], vp[i]) * 
                                 _matrix_element(c_op_fmmesolve, vp[i], vp[j]) * 
                                 noise_spectrum(ep[j] - ep[i])
@@ -128,8 +127,42 @@ end
 
 end
 
-#   NEXT:  Test Floquet-Markov Master Equation for a two-level system 
-#         subject to dissipation with internal transform of fmmesolve
+
+@testset "Floquet Master Equation" begin
+    for kmax in (5, 25, 100)
+        delta = 1.0 * 2π
+        eps0 = 1.0 * 2π
+        A = 0.0 * 2π
+        omega = sqrt(delta^2 + eps0^2)
+        T = 2π / omega
+        tlist = range(0.0, 2 * T, 101)
+        psi0 = rand_ket(2)
+        H0 = - eps0 / 2 * sigmaz() - delta / 2 * sigmax()
+        H1 = A/2.0 * sigmax()
+        H = (H0, (H1, t -> sin(omega * t)))
+        e_ops = [num(2)]
+        gamma1 = 1.0
+
+        # Collapse operator for Floquet-Markov Master Equation
+        c_op_fmmesolve = sigmax()
+
+        # Collapse operator for Lindblad Master Equation
+        @inline spectrum(ω::Real) = ifelse(ω>0, ω * 0.5 * gamma1 / (2π), zero(ω))
+
+        (ep, vp) = eigenstates(H0)
+        op0 = vp[1] * vp[1]'
+        op1 = vp[2] * vp[2]'
+
+        c_op_mesolve = _convert_c_ops(c_op_fmmesolve, spectrum, vp, ep)
+
+        #Solve the floquet master equation
+        floquet_basis = FloquetBasis(H, T, tlist)
+        solver = FMESolver(floquet_basis, c_op_fmmesolve, spectrum, kmax=kmax)  
+
+        #Not sure how the FMESolver will work in our case 1) Look into qutip on what is going on and 2) Implement the test properly here
+
+    end
+end
 
 
 
