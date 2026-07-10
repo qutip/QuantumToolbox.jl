005f8ed3333f015eb1d33c02c83becb54cae7270
Yi-Te Huang <yitehuang.tw@gmail.com>
2026-07-10 15:15:09 +0800

    fix typo

--- ext/QuantumToolboxAMDGPUExt.jl
+++ ext/QuantumToolboxAMDGPUExt.jl
@@ -75,32 +75,27 @@ Return a new [`QuantumObject`](@ref) where `A.data` is in the type of `AMDGPU` a
 - `A::QuantumObject`: The [`QuantumObject`](@ref)
 - `word_size::Int`: The word size of the element type of `A`, can be either `32` or `64`. Default to `64`.
 """
-function roc(A::QuantumObject; word_size::Union{Val,Int} = Val(64))
+function roc(A::QuantumObject; word_size::Union{Val, Int} = Val(64))
     _word_size = getVal(makeVal(word_size))
 
     ((_word_size == 64) || (_word_size == 32)) || throw(DomainError(_word_size, "The word size should be 32 or 64."))
 
     return roc(A, makeVal(word_size))
 end
-roc(A::QuantumObject, word_size::Union{Val{32},Val{64}}) =
+roc(A::QuantumObject, word_size::Union{Val{32}, Val{64}}) =
     QuantumObject(adapt(ROCArray{_convert_eltype_wordsize(eltype(A), word_size)}, A.data), A.type, A.dimensions)
 function roc(
-    A::QuantumObject{ObjType,DimsType,<:SparseVector},
-    word_size::Union{Val{32},Val{64}},
-) where {ObjType<:QuantumObjectType,DimsType<:AbstractDimensions}
+        A::QuantumObject{ObjType, DimsType, <:SparseVector},
+        word_size::Union{Val{32}, Val{64}},
+    ) where {ObjType <: QuantumObjectType, DimsType <: Dimensions}
     return ROCSparseVector{_convert_eltype_wordsize(eltype(A), word_size)}(A)
 end
 function roc(
-    A::QuantumObject{ObjType,DimsType,<:SparseMatrixCSC},
-    word_size::Union{Val{32},Val{64}},
-) where {ObjType<:QuantumObjectType,DimsType<:AbstractDimensions}
+        A::QuantumObject{ObjType, DimsType, <:SparseMatrixCSC},
+        word_size::Union{Val{32}, Val{64}},
+    ) where {ObjType <: QuantumObjectType, DimsType <: Dimensions}
     return ROCSparseMatrixCSC{_convert_eltype_wordsize(eltype(A), word_size)}(A)
 end
-
-QuantumToolbox.to_dense(A::MT) where {MT<:AbstractROCSparseArray} = ROCArray(A)
-
-QuantumToolbox.to_dense(::Type{T1}, A::ROCArray{T2}) where {T1<:Number,T2<:Number} = ROCArray{T1}(A)
-QuantumToolbox.to_dense(::Type{T}, A::AbstractROCSparseArray) where {T<:Number} = ROCArray{T}(A)
 
 QuantumToolbox._sparse_similar(A::ROCSparseMatrixCSC, args...) = sparse(args..., fmt = :csc)
 QuantumToolbox._sparse_similar(A::ROCSparseMatrixCSR, args...) = sparse(args..., fmt = :csr)
