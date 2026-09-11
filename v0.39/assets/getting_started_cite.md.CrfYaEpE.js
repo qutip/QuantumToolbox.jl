faa133092f65bead878c0910bb444960709670c8
Documenter.jl <documenter@juliadocs.github.io>
2026-09-11 11:13:19 +0000

    delete history

---
+++ v0.39/assets/getting_started_cite.md.CrfYaEpE.js
@@ -0,0 +1,13 @@
+import{_ as n,c as s,o as e,aA as t}from"./chunks/framework.BtR_ZV87.js";const m=JSON.parse('{"title":"Cite QuantumToolbox.jl","description":"","frontmatter":{},"headers":[],"relativePath":"getting_started/cite.md","filePath":"getting_started/cite.md","lastUpdated":null}'),i={name:"getting_started/cite.md"};function p(o,a,l,r,c,u){return e(),s("div",null,[...a[0]||(a[0]=[t(`<h1 id="doc:Cite" tabindex="-1">Cite QuantumToolbox.jl <a class="header-anchor" href="#doc:Cite" aria-label="Permalink to &quot;Cite QuantumToolbox.jl {#doc:Cite}&quot;">​</a></h1><p>If you like <code>QuantumToolbox.jl</code>, we would appreciate it if you could cite our publication [ <a href="https://doi.org/10.22331/q-2025-09-29-1866" target="_blank" rel="noreferrer">Quantum 9, 1866 (2025)</a> ] using the following bibtex entry:</p><div class="language-bib vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bib</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>@article{QuantumToolbox.jl2025,</span></span>
+<span class="line"><span>  title = {Quantum{T}oolbox.jl: {A}n efficient {J}ulia framework for simulating open quantum systems},</span></span>
+<span class="line"><span>  author = {Mercurio, Alberto and Huang, Yi-Te and Cai, Li-Xun and Chen, Yueh-Nan and Savona, Vincenzo and Nori, Franco},</span></span>
+<span class="line"><span>  journal = {{Quantum}},</span></span>
+<span class="line"><span>  issn = {2521-327X},</span></span>
+<span class="line"><span>  publisher = {{Verein zur F{\\&quot;{o}}rderung des Open Access Publizierens in den Quantenwissenschaften}},</span></span>
+<span class="line"><span>  volume = {9},</span></span>
+<span class="line"><span>  pages = {1866},</span></span>
+<span class="line"><span>  month = sep,</span></span>
+<span class="line"><span>  year = {2025},</span></span>
+<span class="line"><span>  doi = {10.22331/q-2025-09-29-1866},</span></span>
+<span class="line"><span>  url = {https://doi.org/10.22331/q-2025-09-29-1866}</span></span>
+<span class="line"><span>}</span></span></code></pre></div>`,3)])])}const _=n(i,[["render",p]]);export{m as __pageData,_ as default};
