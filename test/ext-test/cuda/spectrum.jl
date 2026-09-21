f60d096713bd566f3bccbbf8f3c9ac315a467bc0
Yi-Te Huang <yitehuang.tw@gmail.com>
2026-09-21 23:02:21 +0800

    update `CUDA` tests

---
+++ test/ext-test/cuda/spectrum.jl
@@ -0,0 +1,37 @@
+using Test
+using QuantumToolbox
+using CUDA
+using CUDA.cuSPARSE
+
+@testset "CUDA (Spectrum)" begin
+    N = 10
+    Id = qeye(N)
+    a = destroy(N) |> cuSPARSE.CuSparseMatrixCSR
+    H = a' * a
+    c_ops = [sqrt(0.1 * (0.01 + 1)) * a, sqrt(0.1 * (0.01)) * a']
+    solver = Lanczos()
+
+    t_l = range(0, 333 * π, length = 1000)
+    corr1 = correlation_2op_1t(H, nothing, t_l, c_ops, a', a; progress_bar = Val(false))
+    corr2 = correlation_3op_1t(H, nothing, t_l, c_ops, Id, a', a; progress_bar = Val(false))
+    ω_l1, spec1 = spectrum_correlation_fft(t_l, corr1)
+
+    ω_l2 = range(0, 3, length = 1000)
+    spec2 = spectrum(H, ω_l2, c_ops, a', a; solver = solver)
+
+    spec1 = spec1 ./ maximum(spec1)
+    spec2 = collect(spec2) ./ maximum(spec2)
+
+    test_func1 = maximum(real.(spec1)) * (0.1 / 2)^2 ./ ((ω_l1 .- 1) .^ 2 .+ (0.1 / 2)^2)
+    test_func2 = maximum(real.(spec2)) * (0.1 / 2)^2 ./ ((ω_l2 .- 1) .^ 2 .+ (0.1 / 2)^2)
+    idxs1 = test_func1 .> 0.05
+    idxs2 = test_func2 .> 0.05
+    @test sum(abs2.(spec1[idxs1] .- test_func1[idxs1])) / sum(abs2.(test_func1[idxs1])) < 0.01
+    @test sum(abs2.(spec2[idxs2] .- test_func2[idxs2])) / sum(abs2.(test_func2[idxs2])) < 0.01
+    @test all(corr1 .≈ corr2)
+
+    # TODO: Fix this
+    # @testset "Type Inference spectrum" begin
+    #     @inferred spectrum(H, ω_l, c_ops, a', a; solver = solver)
+    # end
+end
