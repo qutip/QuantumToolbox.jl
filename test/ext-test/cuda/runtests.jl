0c72100e369d8381cc6c9682974c153a17a95aec
Yi-Te Huang <yitehuang.tw@gmail.com>
2026-09-23 18:02:57 +0800

    remove diagnostics

--- test/ext-test/cuda/runtests.jl
+++ test/ext-test/cuda/runtests.jl
@@ -5,21 +5,6 @@ using ParallelTestRunner
 QuantumToolbox.about()
 CUDA.versioninfo()
 
-# TEMPORARY DIAGNOSTIC: confirm whether --code-coverage propagates from this
-# coordinator process to the Malt-spawned workers, and whether the paths
-# involved actually agree. Remove once resolved.
-function _coverage_diagnostic(label)
-    opts = Base.JLOptions()
-    tp = opts.tracked_path == C_NULL ? "<none>" : unsafe_string(opts.tracked_path)
-    @info "$label coverage opts" code_coverage=opts.code_coverage tracked_path=tp pwd=pwd() this_file=@__FILE__
-end
-_coverage_diagnostic("coordinator")
-const init_worker_code = quote
-    opts = Base.JLOptions()
-    tp = opts.tracked_path == C_NULL ? "<none>" : unsafe_string(opts.tracked_path)
-    @info "worker coverage opts" code_coverage=opts.code_coverage tracked_path=tp pwd=pwd() this_file=@__FILE__
-end
-
 testsuite = find_tests(dirname(@__FILE__))
 
 runtests(QuantumToolbox, ARGS; testsuite, init_worker_code)
